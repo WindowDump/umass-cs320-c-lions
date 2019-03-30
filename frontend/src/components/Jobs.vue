cfad455426bf082482fa8ffd8251e0e6e6dc7f66
Naflin <nathanblue00@gmail.com>
2019-03-30 01:41:11 -0400

    Created jobs and job components

---
+++ frontend/src/components/Jobs.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="jobs">
+    <Job
+      v-for="job in joblist"
+      v-bind:key="job.id"
+      v-bind:title="job.title"
+      v-bind:desc="job.desc"
+      v-bind:location="job.location"
+      v-bind:company="job.company"
+      v-bind:dateposted="job.dateposted"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Job from "@/components/Job.vue"; // @ is an alias to /src
+export default Vue.extend({
+  data() {
+    return {
+      joblist: [
+        {
+          title: "Chef",
+          desc: "We're looking for someone really good at making soup.",
+          company: "Magic Soups",
+          location: "Westville",
+          dateposted: "2/24/19"
+        },
+        {
+          title: "Bridge Builder",
+          desc: "Uh, we're building bridges. Halp.",
+          company: "Bwidges",
+          location: "Oakland",
+          dateposted: "2/4/19"
+        },
+        {
+          title: "Astronaut",
+          desc: "Top secret missions in deep space. Ae you ready?",
+          company: "Shhpace",
+          location: "Decker",
+          dateposted: "1/27/19"
+        },
+        {
+          title: "Fisherman",
+          desc: "Catchin' fish.  Not afraid of sharks? Apply now!",
+          company: "Joe's Fishing Crew",
+          location: "Duckston",
+          dateposted: "1/17/19"
+        }
+      ]
+    };
+  },
+  components: {
+    Job
+  }
+});
+</script>
+
+<style lang="scss">
+.jobs {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>Home</h1>
+    <Jobs></Jobs>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-@Component({
+import Vue from "vue";
+import Jobs from "@/components/Jobs.vue";
+export default Vue.extend({
   components: {
-    HelloWorld
+    Jobs
   }
-})
-export default class Home extends Vue {}
+});
 </script>
