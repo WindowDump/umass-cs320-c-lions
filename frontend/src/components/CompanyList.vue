cc68e7837ab46caa802fe4142edcf8ac8c1a28ff
Cory Lanza <corylanza@gmail.com>
2019-04-03 18:00:29 -0400

    Added a companies page that displays a list of companies from the database through axious. Check it out at /companies

---
+++ frontend/src/components/CompanyList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="companylist">
+    <Company
+      v-for="com in comlist"
+      v-bind:key="com.id"
+      v-bind:name="com.name"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Company from '@/components/Company.vue' // @ is an alias to /src
+import Axios from 'axios'
+export default Vue.extend({
+  data() {
+    return {
+      joblist: null
+    }
+  },
+  async mounted() {
+    const { data } = await Axios.get('/companies')
+    // console.log(data)
+    // debugger
+    this.joblist = data
+  },
+  components: {
+    Company
+  }
+})
+</script>
+
+<style lang="scss">
+.jobs {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import JobPostings from './views/JobPostings.vue'
 
 Vue.use(Router)
 
@@ -21,6 +22,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/companies',
+      name: 'companies',
+      component: () => import('./views/Companies.vue')
     }
   ]
 })
