cb9a6cb0afdb57a892d3abb1a9308cc3a3ad2254
Nathan Scheele <nathanscheele@gmail.com>
2019-04-08 19:22:21 -0400

    Updated tests for /companies and /positions
    14/14 tests passing for CRUD functionality

--- test/app.test.ts
+++ test/app.test.ts
@@ -6,7 +6,8 @@ import { Server } from 'http'
 import url from 'url'
 import app from '../src/app'
 
-let id;
+let cid;
+let pid;
 const port = app.get('port') || 3030
 const getUrl = (pathname?: string) => url.format({
   hostname: app.get('host') || 'localhost',
@@ -56,7 +57,6 @@ describe('Feathers application tests', () => {
     })
   })
 
-  
   describe('Tests for /companies', async () => {
     // No companies to start with 
     it('should contain no companies', async () => {
@@ -66,7 +66,7 @@ describe('Feathers application tests', () => {
         method: 'GET'
       })
 
-      assert.ok(res.length === 0, 'db does not contain zero companies')
+      assert.ok(res.length === 0, '/companies does not contain zero companies')
     })
 
     // Add a company
@@ -77,40 +77,41 @@ describe('Feathers application tests', () => {
         method: 'POST',
         body: {name: 'Google'}
       })
-      id = res._id 
+      cid = res._id 
       assert.ok(res !== -1, 'Google was not added')
     })
 
     it('should contain \'Google\'', async () => {
       const res = await rp({
         url: getUrl('/companies'),
-        json: true
+        json: true,
+        method: 'GET'
       })
-      assert.ok(res.length === 1, 'db does not contain one elements')
-      assert.ok(res[0].name === 'Google', 'Only element in db is not \'Google\'')
+      assert.ok(res.length === 1, '/companies does not contain one element')
+      assert.ok(res[0].name === 'Google', 'Google is not in /companies')
     })
 
     // Update a company
-    it('Google is now Microsoft', async () => {
+    it('should now be Microsoft', async () => {
       const res = await rp({
-        url: getUrl(`/companies/${id}`),
+        url: getUrl(`/companies/${cid}`),
         json: true,
         method: 'PATCH',
         body: { name: 'Microsoft' }
       })
 
-      assert.ok(res.name === 'Microsoft', 'Only element in db is not \'Microsoft\'')
+      assert.ok(res.name === 'Microsoft', 'Microsoft is not in /companies')
     })
 
     // Delete a company
-    it('should remove Microsoft from the db', async () => {
+    it('should remove Microsoft from /companies', async () => {
       const res = await rp({
-        url: getUrl(`/companies/${id}`),
+        url: getUrl(`/companies/${cid}`),
         json: true,
         method: 'DELETE'
       })
 
-      assert.ok(res._id === id, '_ids do not match')
+      assert.ok(res._id === cid, '_ids do not match')
       assert.ok(res.name === 'Microsoft', 'Microsoft was not removed')
     })
 
@@ -122,7 +123,84 @@ describe('Feathers application tests', () => {
         method: 'GET'
       })
 
-      assert.ok(res.length === 0, 'db does not contain zero companies')
+      assert.ok(res.length === 0, '/companies does not contain 0 companies')
+    })
+  })
+
+  describe('Tests for /positions', async () => {
+    // No positions to start with 
+    it('should contain no positions', async () => {
+      const res = await rp({
+        url: getUrl('/positions'),
+        json: true,
+        method: 'GET'
+      })
+
+      assert.ok(res.length === 0, '/positions does not contain 0 companies')
+    })
+
+    // Add a position
+    it('should add CEO to /positions', async () => {
+      const res = await rp({
+        url: getUrl('positions'),
+        json: true,
+        method: 'POST',
+        body: {
+        title: 'CEO',
+        description: "runs the company",
+        payRange: 1000000,
+        jobType: 'full-time',
+        startDate: new Date(),
+        postingDate: new Date(),
+        postingExpirationDate: Date()}
+      })
+      pid = res._id 
+      assert.ok(res !== -1, 'CEO was not added')
+    })
+
+    it('should contain CEO', async () => {
+      const res = await rp({
+        url: getUrl('/positions'),
+        json: true,
+        method: 'GET'
+      })
+      assert.ok(res.length === 1, '/positions does not contain 1 element')
+      assert.ok(res[0].title === 'CEO', 'CEO is not in /companies')
+    })
+
+    // Update a position
+    it('should now have a different pay-range', async () => {
+      const res = await rp({
+        url: getUrl(`/positions/${pid}`),
+        json: true,
+        method: 'PATCH',
+        body: { payRange: 500000 }
+      })
+
+      assert.ok(res.payRange === 500000, 'Pay-range was not updated')
+    })
+
+    // Delete a position
+    it('should remove CEO from the /positions', async () => {
+      const res = await rp({
+        url: getUrl(`/positions/${pid}`),
+        json: true,
+        method: 'DELETE'
+      })
+
+      assert.ok(res._id === pid, '_ids do not match')
+      assert.ok(res.title === 'CEO', 'CEO was not removed')
+    })
+
+    // No more companies
+    it('should contain no companies again', async () => {
+      const res = await rp({
+        url: getUrl('/positions'),
+        json: true,
+        method: 'GET'
+      })
+
+      assert.ok(res.length === 0, 'db does not contain zero positions')
     })
   })
 })
