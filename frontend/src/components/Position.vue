9ef1fd462767f8a41eccc6459387531f25633241
Cory Lanza <corylanza@gmail.com>
2019-04-10 13:39:13 -0400

    added a view that lists positions to the add position page

---
+++ frontend/src/components/Position.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="job">
+    <span class="dateposted"
+      >Posted {{ postingDate }} expires: {{ postingExpirationDate }}</span
+    >
+    <div class="job-info">
+      <div class="title">{{ title }}</div>
+      <div class="position-heading">
+        <p>{{ description }}</p>
+      </div>
+      <p>Pay: {{ payRange }}</p>
+      <p>Job Type: {{ jobType }}</p>
+      <p>Start Date: {{ startDate }}</p>
+    </div>
+    <button class="btn-apply" v-on:click="removepos">Delete</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Axios from 'axios'
+export default Vue.extend({
+  props: [
+    'id',
+    'title',
+    'description',
+    'payRange',
+    'jobType',
+    'startDate',
+    'postingDate',
+    'postingExpirationDate'
+  ],
+  data: () => ({}),
+  methods: {
+    removepos: async function(event: Event) {
+      const { data } = await Axios.delete('/positions/' + this.id)
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.job {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  background: rgb(221, 240, 240);
+  padding: 15px;
+  width: 100%;
+  position: relative;
+  margin: 5px;
+}
+.job-info {
+  flex: 1 1;
+  padding-right: 10px;
+}
+.btn-apply {
+  width: 100px;
+  border-radius: 5px;
+  border: none;
+  background: green;
+  color: #fff;
+  font-size: 1rem;
+  padding: 10px;
+  cursor: pointer;
+}
+.btn-apply:hover,
+.btn-apply:focus {
+  background: rgb(5, 150, 5);
+}
+.btn-apply:focus {
+  outline: none;
+}
+.btn-apply:active {
+  transform: scale(0.97);
+}
+.title {
+  font-size: 1.5em;
+  padding-bottom: 10px;
+}
+.position-heading {
+  padding-bottom: 5px;
+}
+.position {
+  font-weight: bold;
+  padding-bottom: 5px;
+}
+.dateposted {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+}
+</style>
--- frontend/src/views/AddPosition.vue
+++ frontend/src/views/AddPosition.vue
@@ -2,15 +2,18 @@
   <div>
     <h1>Add a new Job Listing</h1>
     <AddPositionForm></AddPositionForm>
+    <PositionList></PositionList>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import AddPositionForm from '@/components/AddPositionForm.vue'
+import PositionList from '@/components/PositionList.vue'
 export default Vue.extend({
   components: {
-    AddPositionForm
+    AddPositionForm,
+    PositionList
   }
 })
 </script>
