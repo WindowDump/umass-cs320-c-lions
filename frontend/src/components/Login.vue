f16bde03b00966d40ade545f86f8642bef6c3e48
Nathan Scheele <nathanscheele@gmail.com>
2019-04-19 16:33:09 -0400

    Update Login.vue

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,87 +1,87 @@
 <template>
   <div id="app">
     <v-app id="inspire">
-      <v-container>
-        <v-layout column center>
-          <v-flex>
-            <v-tabs
-              v-model="active"
-              color="cyan"
-              dark
-              slider-color="yellow"
-              centered
-            >
-              <v-tab ripple>Login</v-tab>
-              <v-tab-item column centered>
-                <v-card flat row>
-                  <v-card-text>Email:</v-card-text>
-                  <v-text-field
-                    v-model="loginEmail"
-                    label="Enter email..."
-                    box
-                    required
-                  ></v-text-field>
-                  <v-card-text>Password:</v-card-text>
-                </v-card>
+      <v-tabs v-model="active" color="cyan" dark slider-color="yellow" centered>
+        <v-tab ripple>Login</v-tab>
+        <v-tab-item column centered>
+          <v-container grid-list-md text-xs-center>
+            <v-layout row wrap>
+              <v-flex xs3></v-flex>
+              <v-flex xs3>
+                <v-card-text class="text-xs-right">Email:</v-card-text>
+              </v-flex>
+              <v-flex xs3>
+                <v-text-field
+                  class="text-xs-left"
+                  v-model="loginEmail"
+                  label="Enter email..."
+                  box
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs3></v-flex>
+              <v-flex xs3></v-flex>
+              <v-flex sx3>
+                <v-card-text class="text-xs-right">Password:</v-card-text>
+              </v-flex>
+              <v-flex sx3>
                 <v-text-field
                   v-model="loginPwd"
-                  :type="password"
+                  :type="'password'"
                   label="Enter password..."
                   box
                   required
                 ></v-text-field>
+                <v-flex xs3></v-flex>
+              </v-flex>
+              <v-flex xs12>
                 <v-btn class="btn-login" @click="login" color="success"
                   >Login</v-btn
                 >
-              </v-tab-item>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-tab-item>
 
-              <v-tab ripple>Create Account</v-tab>
-              <v-tab-item>
-                <v-form
-                  ref="form"
-                  @submit="create"
-                  v-model="valid"
-                  lazy-validation
-                >
-                  <v-card flat>
-                    <v-card-text>Email:</v-card-text>
-                    <v-text-field
-                      v-model="createEmail"
-                      :rules="emailRules"
-                      label="Enter email..."
-                      box
-                      required
-                    ></v-text-field>
-                    <v-card-text>Password:</v-card-text>
-                    <v-text-field
-                      v-model="createPwd"
-                      :rules="pwdRules"
-                      :type="'password'"
-                      label="Password must contain at least 1 lowercase, 1 uppercase, 1 numeric, and 1 special character. It must be at least 8 characters long"
-                      box
-                      required
-                    ></v-text-field>
-                    <v-card-text>Confirm Password:</v-card-text>
-                    <v-text-field
-                      v-model="createPwdConfirm"
-                      :rules="pwdRules"
-                      :type="'password'"
-                      label="Confirm password..."
-                      box
-                      required
-                    ></v-text-field>
-                  </v-card>
-                  <v-flex>
-                    <v-btn class="btn-create" type="submit" color="success"
-                      >Create</v-btn
-                    >
-                  </v-flex>
-                </v-form>
-              </v-tab-item>
-            </v-tabs>
-          </v-flex>
-        </v-layout>
-      </v-container>
+        <v-tab ripple>Create Account</v-tab>
+        <v-tab-item>
+          <v-form ref="form" @submit="create" v-model="valid" lazy-validation>
+            <v-card flat>
+              <v-card-text>Email:</v-card-text>
+              <v-text-field
+                v-model="createEmail"
+                :rules="emailRules"
+                label="Enter email..."
+                box
+                required
+              ></v-text-field>
+              <v-card-text>Password:</v-card-text>
+              <v-text-field
+                v-model="createPwd"
+                :rules="pwdRules"
+                :type="'password'"
+                label="Password must contain at least 1 lowercase, 1 uppercase, 1 numeric, and 1 special character. It must be at least 8 characters long"
+                box
+                required
+              ></v-text-field>
+              <v-card-text>Confirm Password:</v-card-text>
+              <v-text-field
+                v-model="createPwdConfirm"
+                :rules="pwdRules"
+                :type="'password'"
+                label="Confirm password..."
+                box
+                required
+              ></v-text-field>
+            </v-card>
+            <v-flex>
+              <v-btn class="btn-create" type="submit" color="success"
+                >Create</v-btn
+              >
+            </v-flex>
+          </v-form>
+        </v-tab-item>
+      </v-tabs>
     </v-app>
   </div>
 </template>
