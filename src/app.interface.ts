74096d9da8b655c8dcd4f688c7a16605e382e6a0
alexhulbert <alex@alexhulbert.com>
2019-05-06 20:38:54 -0400

    Defined typings for services

--- src/app.interface.ts
+++ src/app.interface.ts
@@ -12,4 +12,46 @@ import { Application } from '@feathersjs/express'
     user = 5; // this won't compile, because user is known to be of type User
   });
  */
-export type App = Application<{}>
+export interface IApp {
+  users: {
+    firstName: string
+    lastName: string
+    canImportData: boolean
+
+    email: string
+    password: string
+
+    managedCompanyId: string
+
+    appliedPositionIds: string[]
+    availablePositionIds: []
+  }
+
+  positions: {
+    companyId: string
+
+    parentPositionId: string
+    subordinatePositionIds: string[]
+
+    appliedUserIds: string[]
+    acceptedUserIds: string[]
+    hiredUserId: string
+    hiredUserAnswers: string[]
+
+    title: string
+    description: string
+    payRange: string
+    jobType: string
+
+    startDate: string
+    postingDate: string
+    postingExpirationDate: string
+  }
+
+  companies: {
+    name: string
+    questions: string[]
+  }
+}
+
+export type App = Application<IApp>
