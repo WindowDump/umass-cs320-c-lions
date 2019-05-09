ee7404ecab5049fbdd035ac99fae9762e0bc8043
alexhulbert <alex@alexhulbert.com>
2019-05-09 00:19:04 -0400

    Don't try to cast managedCompanyId to string unless it exists

--- src/hooks/onlyCompanyManager.ts
+++ src/hooks/onlyCompanyManager.ts
@@ -6,7 +6,10 @@ export default function(): Hook {
   return async (context: HookContext<IApp['positions']>) => {
     const user = context.params.user as IApp['users']
     const record = await context.service.get(context.id!)
-    if (!user || user.managedCompanyId.toString() !== record.companyId.toString()) {
+    if (
+      !user || !user.managedCompanyId ||
+      user.managedCompanyId.toString() !== record.companyId.toString()
+    ) {
       context.error = new errors.Forbidden("You are not a manager of this company")
       throw context.error
     }
