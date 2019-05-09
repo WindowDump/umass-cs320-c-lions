a6a8b8d9419e170288827560a01519921d388ac5
syninja <syninja@gmail.com>
2019-05-09 13:37:24 -0400

    method for onboarding. Still need to write questions.

--- frontend/src/components/Onboarding.vue
+++ frontend/src/components/Onboarding.vue
@@ -15,6 +15,20 @@ import Vue from 'vue'
 import Axios from 'axios'
 
 export default Vue.extend({
-  data: () => ({})
+  data: () => ({
+    questions: ['a', 'b', 'c'],
+    answers: []
+  }),
+  methods: {
+    async onboarding_complete() {
+      await Axios.patch('/positions/' + this.$route.params.id, {
+        acceptPosition: true,
+        qa: this.questions.map((q, i) => ({
+          question: q,
+          answer: this.answers[i]
+        }))
+      })
+    }
+  }
 })
 </script>
